768ebd422d874a60e3960f6d4b2357eb55c3a18c
FWS-22-4 <fws>
2025-01-20 22:43:49 +0530

    Code optimization

--- js/filter.js
+++ js/filter.js
@@ -2,11 +2,9 @@ async function gameplatform() {
     const url = `https://free-to-play-games-database.p.rapidapi.com/api/games`;
     try {
         const data = await fetchDataFromAPI(url);
-        // console.log(data);
         const gamecard = document.querySelector('#platform');
         const gamecat = document.querySelector('#category_filter');
         const gameyear = document.querySelector('#release_year');
-        const sortby = document.querySelector('#sort_by');
         const  gamePlatform ={};
         const genreSet = new Set();  
         const gameYear ={};
@@ -36,31 +34,32 @@ async function gameplatform() {
                 year.textContent = yeardata;
                 gameyear.appendChild(year);
             }
+        });
     document.addEventListener('change', (event) => {
         const targetId = event.target.id; // Get the ID of the dropdown that triggered the event
         const selectedValue = event.target.value; // Get the selected value
         const url = new URL(window.location.href);
         const params = new URLSearchParams(url.search);
         switch (targetId) {
-            case 'platform': // Handle platform changes
+            case 'platform': 
                 if (selectedValue.toLowerCase().includes("web")) {
                     params.set('platform', 'browser');
                 } else {
-                    params.set('platform', selectedValue);
+                    params.set('platform', 'pc');
                 }
                 break;
-            case 'category_filter': // Handle category changes
+            case 'category_filter': 
                 params.set('category', selectedValue);
                 break;
-            case 'release_year': // Handle release year changes
+            case 'release_year':
                 params.set('years', selectedValue);
                 break;
-            case 'sort_by': // Handle sort by changes
+            case 'sort_by': 
                 params.set('sort', selectedValue);
                 break;
         }
         window.location.href = `games.html?${params.toString()}`;
-    });
+   
 });
     } catch (error) {
         console.error('Error during initialization:', error);
